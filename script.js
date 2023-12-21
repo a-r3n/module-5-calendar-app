a45986ace3c9aadbc030daeb10966ec4c1f949a4
a-r3n <alec.renehan@outlook.com>
2023-12-21 14:44:12 +1100

    Added more hour blocks to HTML and added remaining Javascript

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -10,36 +9,80 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-// Get all elements with the class "save-button"
-  var saveButtons = document.querySelectorAll('.btn saveBtn col-2 col-md-1');
-
-// Add a click event listener to each save button
-  saveButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
-
-// Get the parent time-block element
-  var timeBlock = this.closest('.col-8 col-md-10 description');
-
-// Extract the id of the time-block
-  var timeBlockId = timeBlock.id;
-
-// For example, save user input in local storage using the time-block id as a key
-  var userInput = timeBlock.querySelector('textarea').value;
-  localStorage.setItem(timeBlockId, userInput);
-
-  console.log('User input saved for ' + timeBlockId);
-  });
-});
+  $(document).ready(function () {
+    // Display current date in the header
+    $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+  
+    // Add click event listener for the save button
+    $(".saveBtn").on("click", function () {
+      // Get the id of the parent time-block
+      var blockId = $(this).parent().attr("id");
+  
+      // Get the user input from the textarea
+      var userInput = $(this).siblings(".description").val();
+  
+      // Save the user input to local storage using the time-block id as a key
+      localStorage.setItem(blockId, userInput);
+    });
+  
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+    // Apply past, present, or future class to each time block
+    function updateBlocks() {
+      // Get the current hour using Day.js
+      var currentHour = dayjs().hour();
+  
+      // Loop through each time block
+      $(".time-block").each(function () {
+        // Extract the hour from the time-block id
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+        // Remove any existing classes
+        $(this).removeClass("past present future");
+  
+        // Add past, present, or future class based on the comparison with current hour
+        if (blockHour < currentHour) {
+          $(this).addClass("past");
+        } else if (blockHour === currentHour) {
+          $(this).addClass("present");
+        } else {
+          $(this).addClass("future");
+        }
+      });
+    }
+  
+    // Call the function to update block classes
+    updateBlocks();
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
   // TODO: Add code to display the current date in the header of the page.
-});
+
+  
+    // Retrieve user input from local storage and set textarea values
+    function retrieveUserInput() {
+      // Loop through each time block
+      $(".time-block").each(function () {
+        // Get the id of the time block
+        var blockId = $(this).attr("id");
+  
+        // Get the user input from local storage using the time-block id as a key
+        var storedInput = localStorage.getItem(blockId);
+  
+        // Set the textarea value with the stored input
+        $(this).find(".description").val(storedInput);
+      });
+    }
+  
+    // Call the function to retrieve user input from local storage
+    retrieveUserInput();
+  });
+  
